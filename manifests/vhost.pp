0b370a722c25119a065e222444c8a22adfd484c8
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2015-08-19 13:33:03 +0200

    test

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -252,7 +252,7 @@ define apache::vhost(
 
   # This ensures that the docroot exists
   # But enables it to be specified across multiple vhost resources
-  if ! defined(File[$docroot]) and $manage_docroot {
+  if $docroot and ! defined(File[$docroot]) and $manage_docroot {
     file { $docroot:
       ensure  => directory,
       owner   => $docroot_owner,
